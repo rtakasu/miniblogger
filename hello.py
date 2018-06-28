e9e764ffe932a0fa22c91f6bfabb79ad55c84d01
Rafael Takasu <rafaeltakasu@Rafaels-MacBook-Pro-3.local>
2018-06-28 07:57:54 -0700

    hello world in bluemix

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import atexit
+import os
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return "Hello World!"
+
+port = int(os.getenv('PORT', 8000))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
