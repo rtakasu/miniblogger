3d8440ea4b614d86d84d1fb3af3ed60e5ad1c520
Rafael Takasu <rafaeltakasu@Rafaels-MacBook-Pro-3.local>
2018-07-01 15:54:11 -0700

    added (hardcoded) table

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ import os
 import json
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello World!"
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 port = int(os.getenv('PORT', 8000))
 
